52888cdc181c523e1abf8c080f3de134543af27b
Mac <mac@Macs-iMac-Pro-2.local>
2023-07-03 15:19:11 +0530

    Form Data

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ const hostname = "127.0.0.1";
 const database = require("./databases/database");
 const userRouter = require("./routers/user");
 const postRouter = require("./routers/post");
-const formidable = require('express-formidable');
 
 if (!config.get('PrivateKey')) {
     console.error('FATAL ERROR: PrivateKey is not defined.');
     process.exit(1);
 }
-app.use(formidable());
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
--- routers/user.js
+++ routers/user.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { registerUser, loginUser, upload, imguploads } = require("../controllers/user");
+const formidable = require('express-formidable');
 
-
-
-router.post("/register", registerUser);
+router.post("/register", formidable(), registerUser);
 router.post("/login", loginUser);
 
 module.exports = router;
