b98a34d93dcb465a6777391dbec9a7afa493f6e5
SnehalButani <snehalbutani03@gmail.com>
2023-07-03 14:54:41 +0530

    minorchanges

--- routers/user.js
+++ routers/user.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const {registerUser,loginUser, upload , imguploads} = require("../controllers/user");
-const middleware = require("../middleware/user");
+const usercheck = require("../middleware/user");
 const fieldimg = require("../middleware/handle-upload");
 const formidable = require('express-formidable');
 
-router.post("/register",formidable(),[middleware,fieldimg],registerUser);
+router.post("/register",formidable(),[usercheck,fieldimg],registerUser);
 router.post("/login",loginUser);
 
 module.exports = router;
